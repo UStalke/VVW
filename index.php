d4944c9028672f1f3f1b43b0e219331dd22310f8
UStalke <53390074+UStalke@users.noreply.github.com>
2019-09-19 11:53:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 
 <?php
 $name = 'du da';
-
 $html = file_get_contents('start.html');
+
+if(isset($_POST['btn'])){
+	$html = str_replace('{success}',"button erfolgreich gedrueckt",$html);
+} else {
+	$html = str_replace('{success}',json_encode($_POST),$html);
+}
+
+
 $html = str_replace('{titel}','Homepage ' . $name,$html);
 $html = str_replace('{name}',$name,$html);
 echo $html; 
+
+
 
 ?>
