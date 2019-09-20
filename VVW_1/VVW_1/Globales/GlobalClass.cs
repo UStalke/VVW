cce82be7cd250f0192fcafbc0828719015022d2d
Jakob <Master-Andres@users.noreply.github.com>
2019-09-20 08:55:03 +0200

    Create GlobalClass.cs

---
+++ VVW_1/VVW_1/Globales/GlobalClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVW_1.Globales
+{
+    class GlobalClass
+    {
+
+        private static int _seiten_nr;
+
+        public static int _SeitenNr { get => _seiten_nr; set => _seiten_nr = value; }
+    }
+}
