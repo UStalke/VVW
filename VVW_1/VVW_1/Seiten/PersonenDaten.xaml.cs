baa142d34bc627c05f564c3f628ccc03550dcbc8
Jakob <Master-Andres@users.noreply.github.com>
2019-09-20 08:58:15 +0200

    Create PersonenDaten.xaml.cs

---
+++ VVW_1/VVW_1/Seiten/PersonenDaten.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VVW_1.Seiten
+{
+    /// <summary>
+    /// Interaktionslogik für PersonenDaten.xaml
+    /// </summary>
+    public partial class PersonenDaten : Page
+    {
+        public PersonenDaten()
+        {
+            InitializeComponent();
+            _FillCBO();
+        }
+
+
+        public void _FillCBO()
+        {
+            Datenbank.SqlLesen _DBL = new Datenbank.SqlLesen();
+            cboanr.ItemsSource =  _DBL._Anrede.DefaultView;
+            cboanr.DisplayMemberPath = _DBL._ContVonPause().Columns[1].ToString();
+            cboanr.SelectedValuePath = _DBL._ContVonPause().Columns[0].ToString();
+        }
+    }
+}
