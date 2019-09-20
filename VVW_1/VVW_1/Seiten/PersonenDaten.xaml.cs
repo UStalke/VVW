5d9d442f44d688b0c27472860e59c4cbde3903ec
Jakob <Master-Andres@users.noreply.github.com>
2019-09-20 13:42:15 +0200

    Update PersonenDaten.xaml.cs

--- VVW_1/VVW_1/Seiten/PersonenDaten.xaml.cs
+++ VVW_1/VVW_1/Seiten/PersonenDaten.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VVW_1.Seiten;
 
 namespace VVW_1.Seiten
 {
@@ -30,9 +31,43 @@ namespace VVW_1.Seiten
         public void _FillCBO()
         {
             Datenbank.SqlLesen _DBL = new Datenbank.SqlLesen();
-            cboanr.ItemsSource =  _DBL._Anrede.DefaultView;
-            cboanr.DisplayMemberPath = _DBL._ContVonPause().Columns[1].ToString();
-            cboanr.SelectedValuePath = _DBL._ContVonPause().Columns[0].ToString();
+            cboanr.ItemsSource =  _DBL._Anrede().DefaultView;
+            cboanr.DisplayMemberPath = _DBL._Anrede().Columns[1].ToString();
+            cboanr.SelectedValuePath = _DBL._Anrede().Columns[0].ToString();
+            cboanr.SelectedIndex = 0;
+
+            cbogeschl.ItemsSource = _DBL._Geschlecht().DefaultView;
+            cbogeschl.DisplayMemberPath = _DBL._Geschlecht().Columns[1].ToString();
+            cbogeschl.SelectedValuePath = _DBL._Geschlecht().Columns[0].ToString();
+            cbogeschl.SelectedIndex = 0;
+
+            cbotil.ItemsSource = _DBL._Titel().DefaultView;
+            cbotil.DisplayMemberPath = _DBL._Titel().Columns[1].ToString();
+            cbotil.SelectedValuePath = _DBL._Titel().Columns[0].ToString();
+            cbotil.SelectedIndex = 0;
+            
+
+
+
+        }
+
+        private void _ChangedInGlobalClassCBO(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboanr.SelectedIndex >= 0) { Globales.GlobalClass._Anrede = Convert.ToInt16(cboanr.SelectedValue.ToString()); }
+            if (cbotil.SelectedIndex >= 0 ){Globales.GlobalClass._Titel = Convert.ToInt16(cbotil.SelectedValue.ToString());}
+            if (cbogeschl.SelectedIndex >= 0) { Globales.GlobalClass._Geschl = Convert.ToInt16(cbogeschl.SelectedValue.ToString()); }
+        }
+
+        private void _ChangedInGlobalClassTXT(object sender, RoutedEventArgs e)
+        {
+            if (txtvname.Text.Length > 0) { Globales.GlobalClass._Vname = txtvname.Text; }
+            if (txtnname.Text.Length > 0) { Globales.GlobalClass._Nname = txtnname.Text; }
+            Globales.GlobalClass._Strasse = txtstrasse.Text;
+            Globales.GlobalClass._Hnr = txthnr.Text;
+            Globales.GlobalClass._Plz = txtplz.Text;
+            Globales.GlobalClass._Ort = txtort.Text;
+            Globales.GlobalClass._Tel = txttel.Text;
+            Globales.GlobalClass._Mobil = txtmobil.Text;
         }
     }
 }
