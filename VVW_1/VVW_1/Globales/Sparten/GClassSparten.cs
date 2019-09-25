742233b07c02824f4daf0bd6b9e9805ca5ebf4aa
Jakob <Master-Andres@users.noreply.github.com>
2019-09-25 09:26:58 +0200

    Create GClassSparten.cs

---
+++ VVW_1/VVW_1/Globales/Sparten/GClassSparten.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVW_1.Globales.Sparten
+{
+    class GClassSparten
+    {
+        private static int[] _checkid = new int[1000];
+
+        private static string _chkSparten;
+        public static int[] _Chksparten { get { return _checkid; } set { _checkid = value; } }
+
+        public static string ChkSparten { get => _chkSparten; set => _chkSparten = value; }
+    }
+}
