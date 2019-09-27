eb102b702bd8b633dc571f7962330fa14aaf0ac1
Jakob <Master-Andres@users.noreply.github.com>
2019-09-27 10:07:19 +0200

    Create SqlSchreiben.cs

---
+++ VVW_1/VVW_1/Datenbank/SqlSchreiben.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace VVW_1.Datenbank
+{
+    class SqlSchreiben
+    {
+        Datenbank.SqlVerbindung _SV = new SqlVerbindung();
+
+        public void _Ins_Temp_ChekBox_Sparten( int _chknr )
+        {
+            SqlCommand _CMD = new SqlCommand();
+            _CMD.Connection = _SV._MsSqlVer();
+            _CMD.CommandType = System.Data.CommandType.StoredProcedure;
+            _CMD.CommandText = "gp_Ins_ChkSparten";
+            _CMD.Parameters.AddWithValue("@guid", Globales.GlobalClass._Sessionid);
+            _CMD.Parameters.AddWithValue("@chk", _chknr);
+            _CMD.Connection.Open();
+            _CMD.ExecuteNonQuery();
+            _CMD.Connection.Close();
+
+        }
+
+        public void _Del_Temp_ChekBox_Sparten(int _chknr)
+        {
+            SqlCommand _CMD = new SqlCommand();
+            _CMD.Connection = _SV._MsSqlVer();
+            _CMD.CommandType = System.Data.CommandType.StoredProcedure;
+            _CMD.CommandText = "gp_Del_ChekSparten";
+            _CMD.Parameters.AddWithValue("@guid", Globales.GlobalClass._Sessionid);
+            _CMD.Parameters.AddWithValue("@chk", _chknr);
+            _CMD.Connection.Open();
+            _CMD.ExecuteNonQuery();
+            _CMD.Connection.Close();
+        }
+
+    }
+}
